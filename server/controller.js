787947350a9149b2a5bd991fa224526aa3975e4f
cdmccune <curtmccune@gmail.com>
2022-04-20 14:40:40 -0700

    completed beginner part of lab

--- server/controller.js
+++ server/controller.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
             rejectUnauthorized: false
         }
     }
-}
+})
 
 let nextEmp = 5
 
@@ -29,8 +29,10 @@ module.exports = {
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`*****YOUR CODE HERE*****
-        
+        sequelize.query(`
+        UPDATE cc_appointments 
+        SET approved = true
+        WHERE appt_id = ${apptId};
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
         (${nextEmp + 1}, ${apptId});
@@ -40,5 +42,46 @@ module.exports = {
                 nextEmp += 2
             })
             .catch(err => console.log(err))
+    },
+
+    getAllClients: (req,res) => {
+        sequelize.query(`
+        SELECT * 
+        FROM cc_users
+        JOIN cc_clients
+        ON cc_users.user_id = cc_clients.user_id;
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+    },
+    getPendingAppointments: (req,res) => {
+        sequelize.query(`
+        SELECT *
+        FROM cc_appointments
+        WHERE approved = false
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+    },
+    getPastAppointments: (req,res) => {
+        sequelize.query(`
+        SELECT a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name
+        FROM cc_appointments AS a
+        JOIN cc_emp_appts AS ea ON a.appt_id = ea.appt_id
+        JOIN cc_employees AS e ON e.emp_id = ea.emp_id
+        JOIN cc_users AS u ON e.user_id = u.user_id
+        WHERE a.approved = true AND a.completed = true
+        ORDER by a.date desc;
+        `)
+        
+        .then(dbRes => res.status(200).send(dbRes[0]))
+    },
+    completeAppointment: (req, res) => {
+        let {apptId} = req.body
+        sequelize.query(`
+        UPDATE cc_appointments
+        SET completed = true
+        WHERE appt_id = ${apptId}
+        `)
+        .then(dbRes => res.status(200).send(dbRes[200]))
     }
+
 }
